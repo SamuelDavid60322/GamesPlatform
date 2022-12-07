9248348dbc9977f075046d90aa76c77dc0b09cc3
SamuelDavid60322 <samueldavid60322@outlook.ie>
2022-12-07 20:56:16 +0000

    Added in repository services in Program file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using GamesPlatform.Data;
+using GamesPlatform.Interfaces;
+using GamesPlatform.Mocks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +21,8 @@ namespace GamesPlatform
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+            builder.Services.AddTransient<IGamesRepository, MockGamesRepository>();
+            builder.Services.AddTransient<ICategoryRepository, ICategoryRepository>();
 
             var app = builder.Build();
 
