fc0a0914f6d010d77ff40af200dccd6e425f4eab
SamuelDavid60322 <samueldavid60322@outlook.ie>
2022-12-07 20:02:50 +0000

    Latest changes to the mock Game repository

--- Mocks/MockGamesRepository.cs
+++ Mocks/MockGamesRepository.cs
@@ -14,13 +14,47 @@ namespace GamesPlatform.Mocks
                 GameName="Kick Ups", 
                 GameDescription= "The goal is simple, keep the ball in the air for the longest amount of time...",
                 Price=20.00M,
-                GameImageUrl ="https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg", },
+                GameImageUrl ="https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                GameImageThumbnailUrl = "https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                IsFeaturedGame = "true"
+            },
 
 
-            new Games{GameID=2, GameName="Mobile Games", 
-                GameDescription= "Avaliable games to play on your mobile device"},
-            new Games{GameID=3, GameName="Casino Games", 
-                GameDescription= "Test your luck in these casino games"}
+            new Games{GameID=2,                
+                GameName="Amoeba Online",
+                GameDescription= "Jump into the ultimate Single cell battle! Compete aganist 100 players or bots in this multiplayer game!",
+                Price=10.00M,
+                GameImageUrl ="https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                GameImageThumbnailUrl = "https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                IsFeaturedGame = "true"
+            },
+
+            new Games{GameID=3,                
+                GameName="Reaction Test Game",
+                GameDescription= "Test your reaction time and compare with others",
+                Price=50.00M,
+                GameImageUrl ="https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                GameImageThumbnailUrl = "https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                IsFeaturedGame = "true"
+            },
+
+            new Games{GameID=4,
+                GameName="Dungeon Deathmatch",
+                GameDescription= "Dunegon Deathmatch is a fast paced top down multiplayer deathmatch game. Fight up to 8 players in this action game!",
+                Price=20.00M,
+                GameImageUrl ="https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                GameImageThumbnailUrl = "https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                IsFeaturedGame = "false"
+            },
+
+            new Games{GameID=5,
+                GameName="DDave Wakes Up",
+                GameDescription= "Dave wakes up is a short narrative top down 2d game. You play as Dave, trapped in a corpo world!",
+                Price=20.00M,
+                GameImageUrl ="https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                GameImageThumbnailUrl = "https://i2-prod.manchestereveningnews.co.uk/sport/football/article24985459.ece/ALTERNATES/s615/0_GettyImages-1413133364.jpg",
+                IsFeaturedGame = "false"
+            },
         };
         public IEnumerable<Games> FeaturedGames { get; set; }
         public IEnumerable<Games> Games { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
