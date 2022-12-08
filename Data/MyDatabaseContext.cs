900720f1b744994ffb09c13b2335ba6c0b9e185f
Samuel David <72495342+SamuelDavid60322@users.noreply.github.com>
2022-12-08 12:38:11 +0000

    Working on database connection

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -1,6 +1,16 @@
-﻿namespace GamesPlatform.Data
+﻿using GamesPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace GamesPlatform.Data
 {
-    public class MyDatabaseContext
+    public class MyDatabaseContext : DbContext
     {
+      public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options): base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
