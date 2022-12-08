65694d870220f9c52e538e9030392f0a0056f139
Samuel David <72495342+SamuelDavid60322@users.noreply.github.com>
2022-12-08 10:23:54 +0000

    Updated Games Controller to see data for the view

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GamesPlatform.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GamesPlatform.Controllers
 {
     public class GamesController :Controller
     {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IGamesRepository _gamesRepository;
 
-        public IActionResult Index()
+        public GamesController(ICategoryRepository categoryRepository, IGamesRepository gamesRepository)
         {
-            return View();
+            _categoryRepository = categoryRepository;
+            _gamesRepository = gamesRepository;
+
+        }
+
+        public ViewResult List() 
+        {
+            var games = _gamesRepository.Games;
+            return View(games);
         }
     }
 }
