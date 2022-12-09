5a902104f0e7ab4828fb6207d1330f5a54ba9c28
SamuelDavid60322 <samueldavid60322@outlook.ie>
2022-12-09 23:50:51 +0000

    Added in View component for Shopping Cart

---
+++ Components/ShoppingCartSummary.cs
@@ -0,0 +1,29 @@
+﻿using GamesPlatform.Models;
+using GamesPlatform.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
+
+namespace GamesPlatform.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+
+        }
+        public IViewComponentResult Invoke()
+        {
+            var items = new List<ShoppingCartItem>() { new ShoppingCartItem(), new ShoppingCartItem() }; //_shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
