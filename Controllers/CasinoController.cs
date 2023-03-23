fd3bc6dfe8cee435f5e261e8867669241b2e5a8b
SamuelDavid60322 <samueldavid60322@outlook.ie>
2023-03-23 22:31:29 +0000

    Began working on Casino

---
+++ Controllers/CasinoController.cs
@@ -0,0 +1,19 @@
+﻿using GamesPlatform.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesPlatform.Controllers
+{
+    public class CasinoController : Controller
+    {
+        private readonly IGamesRepository _gamesRepository;
+        public CasinoController(IGamesRepository gamesRepository)
+        {
+            _gamesRepository = gamesRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
