04d5994d093e6857caa76557da2ea5e8814708ce
Samuel David <72495342+SamuelDavid60322@users.noreply.github.com>
2023-03-28 13:57:26 +0100

    Implemented Play Functionality for Users who purchased Games. Working on Withdraws Next

---
+++ Controllers/PlayGameController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamesPlatform.Controllers
+{
+    public class PlayGameController : Controller
+    {
+        public IActionResult PlayGame(string gameID)
+        {
+            ViewBag.GameID = gameID;
+            return View();
+        }
+    }
+}
